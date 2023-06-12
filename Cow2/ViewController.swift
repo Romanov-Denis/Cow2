007bc213bc55b6d25666b78aabfd0cbdac3c68e7
Romanov-Denis <rnd.romanov@gmail.com>
2023-06-12 18:48:56 +0700

    Initial Commit

---
+++ Cow2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cow2
+//
+//  Created by Денис Романов on 12/6/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
